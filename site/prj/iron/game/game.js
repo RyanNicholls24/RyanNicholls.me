e1ec0df27ccf45700e148e4b035141c3e81c32cb
Ryan Nicholls <ryannicholls24@gmail.com>
2019-10-28 20:18:29 -0700

    initial commit for text based rpg "iron"

---
+++ site/prj/iron/game/game.js
@@ -0,0 +1,12 @@
+//GLOBAL VARS
+var writeSpeed = 50;
+//GLOBAL VARS
+
+function start(){
+ write('choicebox',script.intro)
+ 
+}
+function write(element, text){
+    document.getElementById(element).innerHTML=text;
+        
+}
---
+++ site/prj/iron/game/script.js
@@ -0,0 +1,3 @@
+var script = {
+    intro:"Welcome to the world of Iron. Your story begins in the town of StormHaven."
+}
