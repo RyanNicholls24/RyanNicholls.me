efb0a76812c093aceff5945f5e218c5af2bb1891
Ryan Nicholls <ryannicholls24@gmail.com>
2019-10-29 15:36:13 -0700

    inital commit for blazor site

---
+++ site/prj/Blazor/BlazorApp1/BlazorApp1/Data/ScriptDict.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class ScriptDict
+    {
+        static void Main()
+        {
+            //Map references, 
+            Dictionary<int, string> Map = new Dictionary<int, string>();
+
+            //Quest Number references
+            Dictionary<int, string> Quest = new Dictionary<int, string>();
+        }
+    }
+}
