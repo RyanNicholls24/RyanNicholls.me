0ea4e4c0f36d86cdaebc65b5431b43c97780512e
Ryan Nicholls <ryannicholls24@gmail.com>
2019-10-31 13:19:04 -0700

    remove unecessary function

--- site/prj/Blazor/BlazorApp1/BlazorApp1/wwwroot/js/JavaScript.js
+++ site/prj/Blazor/BlazorApp1/BlazorApp1/wwwroot/js/JavaScript.js
@@ -6,12 +6,3 @@ function setInnerHTML(id, content) {
 function setElementVis(id, vis) {
     document.getElementById(id).style.visibility = vis;
 }
-
-function createElementNew(type, parentid, childid, content) {
-    var newE = document.createElement(type);
-    newE.id = childid;
-    var node = document.createTextNode(content);
-    newE.appendChild(node);
-    var element = document.getElementById(parentid);
-    element.appendChild(newE);
-}
